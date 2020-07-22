84436f662f2b509d1cde11599adbc2c783d9a217
Unnikrishnan, Anoop <Anoop.Unnikrishnan@McKesson.com>
2020-07-22 12:43:10 -0700

    Skeliton springboot  project structure with web and springboot kafka dependency

---
+++ producerservice/src/main/java/com/mckesson/producer/MckessonProducerApplication.java
@@ -0,0 +1,17 @@
+package com.mckesson.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+public class MckessonProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MckessonProducerApplication.class, args);
+	}
+
+}
---
+++ producerservice/src/test/java/com/mckesson/producer/MckessonProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mckesson.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MckessonProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
