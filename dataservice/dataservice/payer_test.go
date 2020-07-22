5b29fc3017a8412c04f87ff3fca7e16cb50c7f54
Shelly Xie <sxie2008@gmail.com>
2020-07-21 17:26:37 -0700

    test case for plan API

--- dataservice/dataservice/payer_test.go
+++ dataservice/dataservice/payer_test.go
@@ -51,4 +51,36 @@ func TestSearchResultPlan(t *testing.T) {
 	if planId == plan[0].planId {
 		fmt.Println("id " + strconv.Itoa(planId) + " matched expection id from DB " + strconv.Itoa(plan[0].planId))
 	}
+
+	var searchType = "PlanPayerId"
+	var planPayerId = 140
+	plan2, err2 := searchResultPlan(searchType, "", planPayerId)
+	if nil == err {
+		//fmt.Println(plan)
+		for i := range plan2 {
+			fmt.Println(plan2[i])
+		}
+	}
+	if nil != err2 {
+		fmt.Println(err2)
+	}
+	if planPayerId == plan2[0].payerId {
+		fmt.Println("input " + strconv.Itoa(planPayerId) + " matches expected from DB " + strconv.Itoa(plan[0].payerId))
+	}
+
+	searchType = "PlanName"
+	var planName = "Elderplan Advantage For Nursing Home Residents Medicare Medicare SN"
+	plan3, err3 := searchResultPlan(searchType, planName, 0)
+	if nil == err {
+		//fmt.Println(plan)
+		for i := range plan3 {
+			fmt.Println(plan3[i])
+		}
+	}
+	if nil != err3 {
+		fmt.Println(err)
+	}
+	if planName == plan3[0].planName {
+		fmt.Println("input " + planName + " matches expected from DB " + plan3[0].planName)
+	}
 }
