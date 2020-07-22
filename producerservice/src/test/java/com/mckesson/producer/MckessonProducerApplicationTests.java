187cbecf693389705b838866aac9684d09ba743e
Unnikrishnan, Anoop <Anoop.Unnikrishnan@McKesson.com>
2020-07-22 16:23:28 -0700

    Adding Springboot Kafka configuration implementation and its Test cases

--- producerservice/src/test/java/com/mckesson/producer/MckessonProducerApplicationTests.java
+++ producerservice/src/test/java/com/mckesson/producer/MckessonProducerApplicationTests.java
@@ -3,6 +3,7 @@ package com.mckesson.producer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.mckesson.producer.config.KafkaConfiguration;
 import com.mckesson.producer.controller.MckessonController;
 import com.mckesson.producer.entities.Message;
 
@@ -24,8 +25,12 @@ class MckessonProducerApplicationTests {
 	@Autowired
 	private MckessonController mckController;
 
+	@Autowired
+	private KafkaConfiguration kafkaConfiguration;
+
 	private static final Logger log = LoggerFactory.getLogger(MckessonProducerApplicationTests.class);
 
+/* ================= Test Cases for  MckessonController====================*/
 	@Test
 	public void testHome() {
 		log.info("Test Default end point....." +mckController.home());
@@ -40,13 +45,32 @@ class MckessonProducerApplicationTests {
 
 	@Test
 	public void testProduce() {
-		log.info("Test Rest end point URL produce=========================>");
+		log.info("Test Rest end point URL produce .......................... .......................... ..........................");
 		Message message = new Message();
 		message.setTopicName("MyTopicName");
 		message.setIncomingMessage( "My IncomingMessage");;
 		mckController.produce(message);
-		log.info("/kafka/produce end point with json ..........................: Success");
+		log.info("/kafka/produce end point with json  ....................................................: Success");
 
+	}
+
+	/* ================= MckessonController KafkaConfiguration ====================*/
+	@Test
+	public void testProducerFactory() {
+		log.info("Test KafkaConfiguration producerFactory......." +kafkaConfiguration.producerFactory());
+		assertNotNull(kafkaConfiguration.producerFactory());
+	}
+
+	@Test
+	public void testProducerConfigurations() {
+		log.info("Test KafkaConfiguration producerConfigurations. ................................" +kafkaConfiguration.producerConfigurations());
+		assertNotNull(kafkaConfiguration.producerConfigurations());
+	}
+
+	@Test
+	public void testKafkaTemplate() {
+		log.info("Test KafkaConfiguration kafkaTemplate.... ............................." +kafkaConfiguration.kafkaTemplate());
+		assertNotNull(kafkaConfiguration.kafkaTemplate());
 	}
 
 }
