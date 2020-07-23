39faceb8efcbbf5d8cbfc86aaf42f98022a85e0b
Unnikrishnan, Anoop <Anoop.Unnikrishnan@McKesson.com>
2020-07-23 16:41:54 -0700

    Adding DTO and Entity

---
+++ producerservice/src/main/java/com/mckesson/producer/dto/PayloadDTO.java
@@ -0,0 +1,47 @@
+package com.mckesson.producer.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PayloadDTO {
+
+    private Long id;
+    @JsonProperty("application_name")
+    private String applicationName;
+    @JsonProperty("incoming_message")
+    private String incomingMessage;
+    @JsonProperty("send_status")
+    private boolean sendStatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIncomingMessage() {
+        return incomingMessage;
+    }
+
+    public void setIncomingMessage(String incomingMessage) {
+        this.incomingMessage = incomingMessage;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
+
+    public boolean isSendStatus() {
+        return sendStatus;
+    }
+
+    public void setSendStatus(boolean sendStatus) {
+        this.sendStatus = sendStatus;
+    }
+
+}
---
+++ producerservice/src/main/java/com/mckesson/producer/entities/Payload.java
@@ -0,0 +1,59 @@
+package com.mckesson.producer.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Payload {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String applicationName;
+    private String incomingMessage;
+    private boolean sendStatus;
+
+    public Payload(String applicationName, String incomingMessage, boolean sendStatus) {
+        this.applicationName = applicationName;
+        this.incomingMessage = incomingMessage;
+        this.sendStatus = sendStatus;
+    }
+
+    public Payload() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
+
+    public String getIncomingMessage() {
+        return incomingMessage;
+    }
+
+    public void setIncomingMessage(String incomingMessage) {
+        this.incomingMessage = incomingMessage;
+    }
+
+    public boolean isSendStatus() {
+        return sendStatus;
+    }
+
+    public void setSendStatus(boolean sendStatus) {
+        this.sendStatus = sendStatus;
+    }
+
+}
