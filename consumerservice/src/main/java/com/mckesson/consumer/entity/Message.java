9365ad0cf3798d5f389937fd2ac23ed0920ebc8a
Jakeer <jakeer.mahamad@mckesson.com>
2020-07-23 13:38:30 -0700

    Added Kafka Configuration and Kafka Consumer

---
+++ consumerservice/src/main/java/com/mckesson/consumer/entity/Message.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.mckesson.consumer.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * @author jakeermahamad
+ *
+ */
+@EntityScan
+public class Message {
+
+	private String appName;
+    private String incomingMessage;
+    private String topicName;
+    
+    
+    
+    
+	public Message(String appName, String incomingMessage) {
+		super();
+		this.appName = appName;
+		this.incomingMessage = incomingMessage;
+	}
+	
+	public String getAppName() {
+		return appName;
+	}
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+	public String getIncomingMessage() {
+		return incomingMessage;
+	}
+	public void setIncomingMessage(String incomingMessage) {
+		this.incomingMessage = incomingMessage;
+	}
+	public String getTopicName() {
+		return topicName;
+	}
+	public void setTopicName(String topicName) {
+		this.topicName = topicName;
+	}
+	
+
+}
