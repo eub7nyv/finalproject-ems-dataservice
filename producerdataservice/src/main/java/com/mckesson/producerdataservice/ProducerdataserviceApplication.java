a2d3c5414459c3d854b8929fa1096d1e11fdb6d1
Unnikrishnan, Anoop <Anoop.Unnikrishnan@McKesson.com>
2020-07-25 16:40:13 -0700

    Skeliton service for producer data service

---
+++ producerdataservice/src/main/java/com/mckesson/producerdataservice/ProducerdataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mckesson.producerdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerdataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerdataserviceApplication.class, args);
+	}
+
+}
