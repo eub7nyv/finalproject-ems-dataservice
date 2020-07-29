1c4710544b7f194c6bdf5c0819f246df5439f6f5
Shelly Xie <sxie2008@gmail.com>
2020-07-29 13:01:36 -0700

    Update compose and build file

--- dataservice/scripts/build.sh
+++ dataservice/scripts/build.sh
@@ -13,9 +13,6 @@ env DBPASS=${DBPASS:-password}
 env DBNAME=${DBNAME:-payer}
 
 export CGO_ENABLED=0 
-# export GOOS=linux 
-# export GOARCH=amd64
+
 go test ${APP_DIR}
 env GOOS=linux GOARCH=amd64 GOFLAGS=-insecure go build -v -installsuffix cgo -o  ${APP_NAME} ${APP_DIR}
-# env GOOS=linux GOARCH=amd64 GOFLAGS=-insecure -host=0.0.0.0  go build -v -installsuffix cgo -o  ${APP_NAME} ${APP_DIR}
-#env GOOS=linux GOARCH=amd64 go install -v  -installsuffix cgo -o ${APP_NAME} ${APP_DIR}
